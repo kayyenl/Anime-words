2c0fc36c767888eb8287da0f195c909a3608c55b
kayyenl <yenyenlee9@gmail.com>
2022-07-15 16:54:43 +0800

    first commit, basic files and still struggling

---
+++ index.js
@@ -0,0 +1,3 @@
+// https://api.pokemontcg.io/v2/types pokemon types
+// https://api.pokemontcg.io/v2/sets/swsh1 pokemon set example
+// https://api.pokemontcg.io/v2/cards/<id> getting a pokemon card
