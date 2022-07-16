bf3223086d29452b632c8b0aa610a233dbe94728
kayyenl <yenyenlee9@gmail.com>
2022-07-17 00:45:46 +0800

    might decide to do more additions

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 const targetArea = document.querySelector(".posts");
-const loadingPageOne = `<div class="posts__no-quotes">
-                            <figure class="body__no-quotes--loading">
-                                <img src="anime-run.gif" alt="" class="posts__no-quotes--img">
-                            </figure>
-                            <span class="body__no-quotes--span">We are hunting down the quotes right now, master!</span>
-                        </div>`
-const loadingPageTen = `<div class="posts__no-quotes">
-                            <figure class="body__no-quotes--loading">
-                                <img src="hideri-anime.gif" alt="" class="posts__no-quotes--img">
-                            </figure>
-                            <span class="body__no-quotes--span">We are hunting down the quotes right now, master!</span>
-                        </div>`
-
 
 async function generateOne() {
-    targetArea.innerHTML = loadingPageOne
+    targetArea.innerHTML = loadingStringGenerator("anime-run.gif")
     const quote = await fetch('https://animechan.vercel.app/api/random');
     const elem = await quote.json();
     targetArea.innerHTML = `<div class="post__container">
-    ${elem.quote}
+    "${elem.quote}"
     <span class="char__show">-${elem.character}, from <span class="show__italics">${elem.anime}</span> </span>
     </div>`
 }
 
 async function generateTen() {
-    targetArea.innerHTML = loadingPageTen
+    targetArea.innerHTML = loadingStringGenerator("hideri-anime.gif")
     const quote = await fetch('https://animechan.vercel.app/api/quotes');
     const quoteJson = await quote.json();
     console.log(typeof quoteJson)
 
     targetArea.innerHTML = quoteJson.map(elem => {
         return `<div class="post__container">
-                    ${elem.quote}
+                    "${elem.quote}"
                     <span class="char__show">-${elem.character}, from <span class="show__italics">${elem.anime}</span> </span>
                 </div>`
     }).join("")
+}
+
+function loadingStringGenerator(gif) {
+    return `<div class="posts__no-quotes">
+    <figure class="body__no-quotes--loading">
+        <img src=${gif} alt="" class="posts__no-quotes--img">
+    </figure>
+    <span class="body__no-quotes--span">We are hunting down the quotes right now, master!</span>
+    </div>`
 }
