78965d8fb0172519fa10318d1c8a4fb1aa27dfce
kayyenl <yenyenlee9@gmail.com>
2022-07-17 00:09:04 +0800

    finally done my first api pulling and formatting

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const targetArea = document.querySelector(".posts");
 
 async function generateOne() {
-
+    const quote = await fetch('https://animechan.vercel.app/api/random');
+    const elem = await quote.json();
+    targetArea.innerHTML = `<div class="post__container">
+                    ${elem.quote}
+                    <span class="char__show">-${elem.character}, from <span class="show__italics">${elem.anime}</span> </span>
+                </div>`
 }
 
 async function generateTen() {
-    
+
 }
