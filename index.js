4e75933460fc8d9b8fb35713bcf8c7ab7242ea2b
kayyenl <yenyenlee9@gmail.com>
2022-07-17 21:14:03 +0800

    tried to implement a search bar but it isnt good

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ async function generateTen() {
     targetArea.innerHTML = loadingStringGenerator("hideri-anime.gif")
     const quote = await fetch('https://animechan.vercel.app/api/quotes');
     const quoteJson = await quote.json();
-    console.log(typeof quoteJson)
+
+    targetArea.innerHTML = quoteJson.map(elem => {
+        return `<div class="post__container">
+                    "${elem.quote}"
+                    <span class="char__show">-${elem.character}, from <span class="show__italics">${elem.anime}</span> </span> 
+                </div>`
+    }).join("")
+}
+
+async function onSearchChange(event) {
+    targetArea.innerHTML = loadingStringGenerator("hideri-anime.gif")
+    const quote = await fetch(`https://animechan.vercel.app/api/quotes/character?name=${event.target.value}`);
+    const quoteJson = await quote.json();
 
     targetArea.innerHTML = quoteJson.map(elem => {
         return `<div class="post__container">
